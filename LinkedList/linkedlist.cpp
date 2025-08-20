a8150646cde1ec470f7cc3e4e386bac748af8ca5
M_HASNAIN_ALI_SHAH <23-cs-51@students.uettaxila.edu.pk>
2025-08-20 22:38:35 +0500

    insert in linkedlist

--- LinkedList/linkedlist.cpp
+++ LinkedList/linkedlist.cpp
@@ -4,60 +4,108 @@ using namespace std;
 class Node {
 public:
     int data;
-    Node* next;
+    Node *next;
 
-    Node(int val) {
+    Node(int val)
+    {
         data = val;
         next = nullptr;
     }
+
+    ~Node(){
+        if(next != nullptr){
+            delete next;
+            next = nullptr;
+        }
+    }
 };
 
-class List {
+class List
+{
 public:
-    Node* head;
-    Node* tail;
+    Node *head;
+    Node *tail;
 
-    List() {
+    List()
+    {
         head = nullptr;
         tail = nullptr;
     }
 
-    void push_front(int val) {
-        Node* newNode = new Node(val);
-        if (head == nullptr) {
+    ~List() {
+        if(head != nullptr) {
+            delete head;
+            head = nullptr;
+        }
+    }
+
+    void push_front(int val)
+    {
+        Node *newNode = new Node(val);
+        if (head == nullptr)
+        {
             head = tail = newNode;
-        } else {
+        }
+        else
+        {
             newNode->next = head;
             head = newNode;
         }
     }
 
-    void push_back(int val) {
-        Node* newNode = new Node(val);
-        if (head == nullptr) {
+    void push_back(int val)
+    {
+        Node *newNode = new Node(val);
+        if (head == nullptr)
+        {
             head = tail = newNode;
-        } else {
+        }
+        else
+        {
             tail->next = newNode;
             tail = newNode;
         }
     }
 
-    void printList() {
-        Node* temp = head;
-        while (temp != nullptr) {
+    void printList()
+    {
+        Node *temp = head;
+        while (temp != nullptr)
+        {
             cout << temp->data << " -> ";
             temp = temp->next;
         }
         cout << "NULL" << endl;
     }
+
+    void insert(int val, int pos)
+    {
+        Node *newNode = new Node(val);
+
+        Node *temp = head;
+        for (int i = 0; i < pos - 1; i++)
+        {
+            temp = temp->next;
+        }
+
+        newNode->next = temp->next;
+        temp->next = newNode;
+    }
+    
 };
 
-int main() {
+int main()
+{
     List ll;
     ll.push_front(3);
     ll.push_front(2);
     ll.push_front(1);
 
+    ll.push_back(4);
+    ll.push_back(5);
+    ll.push_back(6);
+
+    ll.insert(100,2);
     ll.printList();
     return 0;
 }
