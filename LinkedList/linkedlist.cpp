bd80586ad70a586e9271905f7d29863e2a67154b
M_HASNAIN_ALI_SHAH <23-cs-51@students.uettaxila.edu.pk>
2025-08-20 19:34:55 +0500

    create push front and push back and print linkedlst

--- LinkedList/linkedlist.cpp
+++ LinkedList/linkedlist.cpp
@@ -1,61 +1,63 @@
-#include<iostream>;
+#include <iostream>
 using namespace std;
 
 class Node {
-    public:
-        int data;
-        Node* next;
+public:
+    int data;
+    Node* next;
 
-        Node(val) {
-            data = val;
-            next = null;
-        }
-}
+    Node(int val) {
+        data = val;
+        next = nullptr;
+    }
+};
 
 class List {
-    public:
-        Node* head;
-        Node* tail;
+public:
+    Node* head;
+    Node* tail;
 
-        List() {
-            head = null;
-            tail = null;
-        }
+    List() {
+        head = nullptr;
+        tail = nullptr;
+    }
 
-        void push_front(val) {
-            Node* newNode = new Node(val)
-            if(head == null){
-                head = tail = newNode;
-            } else {
-                newNode->next = head;
-                head = newNode;
-            }
+    void push_front(int val) {
+        Node* newNode = new Node(val);
+        if (head == nullptr) {
+            head = tail = newNode;
+        } else {
+            newNode->next = head;
+            head = newNode;
         }
+    }
 
-        void push_back(val) {
-            Node* newNode = new Node(val)
-            if(head == null){
-                head = tail = newNode;
-            } else {
-                tail->next = newNode
-                tail = newNode
-            }
+    void push_back(int val) {
+        Node* newNode = new Node(val);
+        if (head == nullptr) {
+            head = tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
         }
+    }
 
-        void printList(){
-            Node* temp = head;
-             while(temp!=null){
-                count<<temp->data<< " -> ";
-                temp = temp->next'
-             }
+    void printList() {
+        Node* temp = head;
+        while (temp != nullptr) {
+            cout << temp->data << " -> ";
+            temp = temp->next;
         }
-}
+        cout << "NULL" << endl;
+    }
+};
 
 int main() {
-    List ll();
-    ll.push_front(3)
-    ll.push_front(2)
-    ll.push_front(1)
+    List ll;
+    ll.push_front(3);
+    ll.push_front(2);
+    ll.push_front(1);
 
     ll.printList();
+    return 0;
 }
