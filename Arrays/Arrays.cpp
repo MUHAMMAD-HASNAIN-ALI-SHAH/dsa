83a77a718dd5113fad7771db5e88d2638c915084
M_HASNAIN_ALI_SHAH <23-cs-51@students.uettaxila.edu.pk>
2025-08-22 14:52:53 +0500

    some more arrays problems

--- Arrays/Arrays.cpp
+++ Arrays/Arrays.cpp
@@ -79,9 +79,27 @@ void printSubArrays(int *arr, int n)
     }
 }
 
+void maxProfit(int *prices, int n)
+{
+    int bestBuy[100000];
+    bestBuy[0] = INT_MAX;
+    for (int i = 1; i < n; i++)
+    {
+        bestBuy[i] = min(bestBuy[i - 1], prices[i - 1]);
+    }
+    int maxProfit = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int currProfit = prices[i] - bestBuy[i];
+        maxProfit = max(maxProfit, currProfit);
+    }
+
+    cout<<"Max profit : "<<maxProfit<<endl;
+}
+
 int main()
 {
-    int arr[6] = {2, -3, 6, -5, 4, 2};
+    int arr[6] = {7, 1, 5, 3, 6, 4};
     int n = 6;
-    maxSubArraySum2(arr, n);
+    maxProfit(arr, n);
 }
